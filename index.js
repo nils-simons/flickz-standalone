39a05a8503a26b2f8c33e76e19f37dec6bf8cdef
Nils Simons <nilssimons707@gmail.com>
2024-11-06 17:43:54 +0100

    1.*

---
+++ index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+require('dotenv').config()
+
+
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './public'), {
+    extensions: ['html'],
+}));
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Flickz *:${process.env.PORT}`);
+});
